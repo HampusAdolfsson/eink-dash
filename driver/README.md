1e764f62a01fc441ca46c8166c10114a39674ec5
Hampus Adolfsson <hampus@adolfsson.email>
2026-05-03 12:47:47 +0200

    [driver] add server script

--- driver/README.md
+++ driver/README.md
@@ -1,6 +1,7 @@
 # driver
 
-This modules is responsible for communicating with the e-ink display.
+This module contains the code that *needs* to run on the Raspberry Pi to drive the e-ink display.
+It uses the `waveshare_epd` library to communicate with the display.
 It currently only supports the 7.5 inch 4-color display, but can easily be extended to support other types of displays by replacing the `waveshare_epd` library.
 
 ## Usage
@@ -8,11 +9,26 @@ It currently only supports the 7.5 inch 4-color display, but can easily be exten
 To show an image on the display:
 
 ```bash
-uv run main.py <image_path>
+uv run draw.py <image_path>
 ```
 
 To clear the display:
 
 ```bash
 uv run clear.py
+```
+
+The `server.py` file runs a simple HTTP server that listens for PUT requests
+containing an image. When an image is received, it is displayed on the e-ink
+display.
+
+```bash
+uv run server.py --port <port_number>
+```
+Where `<port_number>` is the port on which the server will listen for incoming requests. The default is 80.
+
+You can send an image to the server using `curl`:
+
+```bash
+curl http://<ip_address>:<port_number>/ --upload-file <image_path>
 ```
---
+++ driver/server.py
@@ -0,0 +1,45 @@
+import argparse
+import http.server
+import subprocess
+import tempfile
+import sys
+
+
+class RequestHandler(http.server.BaseHTTPRequestHandler):
+    def do_PUT(self):
+        try:
+            content_length = int(self.headers["Content-Length"])
+            content_type = self.headers["Content-Type"]
+            if content_type == "image/png":
+                extension = "png"
+            elif content_type == "image/jpeg":
+                extension = "jpg"
+            else:
+                extension = self.path.split(".")[-1]
+            image_data = self.rfile.read(content_length)
+        except Exception as e:
+            self.send_error(400, str(e))
+
+        with tempfile.NamedTemporaryFile(suffix=f".{extension}") as tmp_file:
+            tmp_file.write(image_data)
+            tmp_file.flush()
+
+            res = subprocess.run(["uv", "run", "draw.py", tmp_file.name])
+            if res.returncode != 0:
+                print("Failed to update display", file=sys.stderr)
+                self.send_error(500, "Failed to update display")
+            self.send_response_only(200)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Run the e-ink display server.")
+    parser.add_argument("--port", type=int, default=80, help="Port to listen on")
+    args = parser.parse_args()
+
+    server_address = ("", args.port)
+    server = http.server.HTTPServer(server_address, RequestHandler)
+    server.serve_forever()
+
+
+if __name__ == "__main__":
+    main()
